e4bf2780345de9c11b0ac460b2ec69905d3e936d
Mojtaba Babaei <babayi.mojtaba.mb@gmail.com>
2025-01-24 18:49:56 +0330

    Add layers config

--- keyboards/keychron/k2_pro/ansi/white/keymaps/guiltiter/config.h
+++ keyboards/keychron/k2_pro/ansi/white/keymaps/guiltiter/config.h
@@ -1,5 +1,12 @@
 #pragma once
 
-#define IGNORE_MOD_TAP_INTERRUPT
-#undef PERMISSIVE_HOLD
-#undef PREVENT_STUCK_MODIFIERS
+/* basic configs */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+/* key hold/press configs */
+#define TAPPING_TERM 160
+#define QUICK_TAP_TERM 100
+#define TAPPING_TOGGLE 2
+// #define IGNORE_MOD_TAP_INTERRUPT obsolete
+// #define RETRO_TAPPING
+// #define PERMISSIVE_HOLD
